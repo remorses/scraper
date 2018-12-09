970c5085f71058bcbb7fb729a93a5292237f2b7b
Tommaso De Rossi <beats.by.morse@gmail.com>
2018-12-09 18:53:52 +0100

    better

--- model.js
+++ model.js
@@ -1,23 +1,32 @@
 
 
-let TitleLink = new Scrape({
+let TitleLink = new ScrapeMany({
     where: '.class > thing', 
     what: 'href', 
     when: elem => elem.coontent() == 'thing',
     how: content => content.strip()
 }
 
-let Titles = new ScrapeList({
-    howMany: '1',
+let Title = new ScrapeMany({
     where: '.class > thing', 
     what: 'href', 
-    when: elem => elem.coontent() == 'thing',
+    onlyWhen: elem => elem.coontent() == 'thing',
     how: content => content.strip()
+}
+
+let Page = new ScrapeOne({
+    selector: '.class', 
+    attribute: 'content', 
+    transform: content => content.strip(),
+    onlyIf: elem => elem.coontent() == 'thing',
+    childOf: Title,
+    parentOf: Thing,
 }
 
 const model = {
-    title: new Scrape('.class', 'content'),
-    link: TitleLink
+    titles: [Title],
+    links: [TitleLink],
+    page: Page
 }
 
 
